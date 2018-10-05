b8336e0ed2ac4921ac1376d0da7192d067b6b721
regafw <43503979+regafw@users.noreply.github.com>
2018-10-05 10:09:55 +0700

    resgitrasi.php
    Rega Febbyan Windiari
    6701170042
    D3MI-41-04

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="number" name="nim" length="10"></td>
+		</tr>
 
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" length="25"></td>
+		</tr>
 
+		<tr>
+			<td>E-mail</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
 
-</body>
-</html>
+		<tr>
+			<td><input type="submit" name="kirim"></td>
+		</tr>		
+	</table>
+</form>
