92c5fddbb5882a1d1f316289370a023219306c54
regafw <43503979+regafw@users.noreply.github.com>
2018-10-05 10:24:41 +0700

    komentar.php
    Rega Febbyan Windiari
    6701170042
    D3MI-41-04

--- komentar.php
+++ komentar.php
@@ -1,3 +1,19 @@
+<form method="POST">
+	komentar : <input type="text" name="komentar"> $nbsp;
+	<input type="submit" name="submit" value="cek">
+
+</form>
+
 <?php
+	if(isset($_POST['submit'])) {
+		$komentar=$_POST['komentar'];
+
+		if(str_word_count($komentar)>==5){
+			echo $komentar;
+		}else if(str_word_count($komentar)<5){
+			echo"error, kata kurang dari 5";
+		}
+
+	}
 
 ?>
